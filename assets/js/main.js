047089b655dfa8d254552afbbaed0bedf8648710
caiocipriano <caio.silvax@hotmail.com>
2022-05-04 10:00:58 -0300

    Scroll Header JS

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY>=30)header.classList.add('scroll-header');
+
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+
